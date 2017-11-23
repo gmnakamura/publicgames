413eb31968d6b4ec538b2dd97f20f20118523904
gmnakamura <gilberto.nakamura@gmail.com>
2017-11-23 10:23:45 -0200

    added argparse

--- single/without_punishment/data_analysis.jl
+++ single/without_punishment/data_analysis.jl
@@ -14,8 +14,8 @@ end
 function autocorrelation_time(data; size=20)
     # c(t) ~ exp(t/tau)
     value  = 1
-    y      = log(autocorrelation(data)[1:size])
-    #y      = log(abs(autocorrelation(data)[1:size]))
+    #y      = log(autocorrelation(data)[1:size])
+    y      = log(abs(autocorrelation(data)[1:size]))
     coeffs = Polynomials.coeffs( Polynomials.polyfit(1:size,y,1))
     if abs(coeffs[end]) > 0
         value = -round(Int64,1.0/coeffs[end])
@@ -51,24 +51,5 @@ function sampling(data; mode="correlated")
     end
     return avg,error # *1.96
 end
-#-----------------------------------------
-function write_data(path, betas, data , error)
-    # write data and error...
-    writedlm(path,zip(betas,data,error))
-    # write gnuplot file
-    gnufilename=string(path[1:end-3],"gnu")
-    f0,f1 = splitdir(path)
-    #texfilename=string(f1[1:end-3],"tex")
-    #texfiles are pick concering dots...
-    texfilename=string(replace(f1,".",""),".tex")
-    open(gnufilename,"w") do f
-        write(f,"#!/usr/bin/gnuplot \n")
-        write(f,"set terminal epslatex standalone color 12\n")
-        write(f,"set output '$texfilename'\n")
-        write(f,"set style fill transparent solid 0.25 \n")
-        write(f,"set xlabel '\$\\beta\$'\n")
-        write(f,"set ylabel '\$n\$' \n")
-        write(f,"plot '$f1' u 1:(\$2-\$3):(\$2+\$3) w filledcurves lc 1 notitle, '' u 1:2 w linespoints pt 7 lc 1 notitle  \n")
-    end
-end
+
 #end #end module
--- single/without_punishment/main.jl
+++ single/without_punishment/main.jl
@@ -5,15 +5,16 @@
 
 include("update.jl")
 include("data_analysis.jl")
+include("helper.jl")
 
-function main(N::Int,montecarlo_steps::Int,betas::Array{Float64,1},
-              args...)
-    # args[1] = b
-    # args[2] = c
+function main(betas::Array{Float64,1},N::Int,
+              montecarlo_steps::Int,args...)
+    # args[1] = b   # b =  profit
+    # args[2] = c   # c =  costs
+    # mu = cooperation risk
 
-    # b, c, mu
+    # b, c, mu 
     couplings=(args[1],args[2],args[2]-args[1]/N)
-    #betas=[beta_min:beta_delta:beta_max]
     avg=zeros(length(betas))
     err=zeros(length(betas))
 
@@ -23,7 +24,7 @@ function main(N::Int,montecarlo_steps::Int,betas::Array{Float64,1},
         beta      = enum_beta[2]
         stat=zeros(montecarlo_steps)
         for step in 1:montecarlo_steps
-            k = rand(1:N)
+            k          = rand(1:N)
             conf[k]    = generate(beta,conf[k],couplings...)
             stat[step] = mean(conf)
         end
@@ -33,16 +34,32 @@ function main(N::Int,montecarlo_steps::Int,betas::Array{Float64,1},
     return avg,err
 end
 #------------------------------------------
-N=100 # read from cmd line
-montecarlo_steps = 10*N*N # read from cmd line
-b = 1.0  # read from cmd line
-c = 0.35 # read from cmd line
-folder ="data" # read from cmd line
+# N=100 # read from cmd line
+# montecarlo_steps = 10*N*N # read from cmd line
+# b = 1.0  # read from cmd line
+# c = 0.35 # read from cmd line
+# folder ="data" # read from cmd line
+# betas = collect(0:0.1:5)
+
+args = parse()
+N                = args["n"]
+b                = 1.0
+c                = args["cost"]
+montecarlo_steps = args["steps"]
+beta_min         = args["beta-min"]
+beta_max         = args["beta-max"]
+beta_delta       = args["beta-delta"]
+folder           = args["folder"]
+betas = collect(beta_min:beta_delta:beta_max)
 
 
-betas = collect(0:0.1:5)
+f = open("log_performance.dat","a")
 #for c in float([0.0,0.1,0.2,0.5,1.0,2.0,4.0,8.0])
-data,err=main(N,montecarlo_steps,betas,b,c)
+t = @elapsed data,err=main(betas,N,montecarlo_steps,b,c)
+write(f,"$N $montecarlo_steps $c $t \n")
 path=joinpath(folder,"publicgames-singlegame-N$N-MC$montecarlo_steps-c$c.dat")
 write_data(path,betas,data,err)
 #end
+
+close(f)
+
